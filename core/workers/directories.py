6948113019847ed421b4069a24bda2976bebaabb
Adarsh Shetty <shettyadarsh18@gmail.com>
2021-06-06 22:56:26 +0530

    Fixed path issues

--- core/workers/directories.py
+++ core/workers/directories.py
@@ -7,6 +7,6 @@ class Directories(Task):
 
     def run(self, *args, **kwargs):
         domain = args[0]
-        os.popen(f'ffuf -w "/reconflow/livedomains/{domain}.txt:DOMAIN" -w /usr/src/reconflow/wordlists/dicc.txt '
+        os.popen(f'ffuf -w "/reconflow/livedomains/{domain}_ldomains.txt:DOMAIN" -w /usr/src/reconflow/wordlists/dicc.txt '
                  f'-u DOMAIN/FUZZ -t 4000 -mc 200 -of json -o /reconflow/directories/{domain}_dirs.json').read()
         return domain
--- core/workers/livedomains.py
+++ core/workers/livedomains.py
@@ -7,6 +7,6 @@ class Livedomains(Task):
 
     def run(self, *args, **kwargs):
         domain = args[0]
-        os.popen(f"cat /reconflow/subdomains/{domain}.txt | httpx -silent | tee /reconflow/livedomains/{domain}_ldomains.txt")\
+        os.popen(f"cat /reconflow/subdomains/{domain}_sdomains.txt | httpx -silent | tee /reconflow/livedomains/{domain}_ldomains.txt")\
             .read()
         return domain

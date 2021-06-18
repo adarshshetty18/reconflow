3b8b4b9e4cc1b70736c67d41d6f72ee75aa0c2e0
Adarsh Shetty <shettyadarsh18@gmail.com>
2021-06-18 21:11:23 +0530

    Changed json to html

--- core/workers/directories.py
+++ core/workers/directories.py
@@ -14,5 +14,5 @@ class Directories(Task):
         os.popen(f'ffuf -w "/reconflow/livedomains/{domain}_ldomains.txt:DOMAIN" -w /usr/src/reconflow/wordlists/dicc.txt '
                  f'-u DOMAIN/FUZZ -t 4000 -mc 200 -of html -o /reconflow/directories/{domain}_dirs.html').read()
         updater.bot.send_document(chat_id=chat_id, caption=f"Here is the directories report for {domain}:",
-                                  document=open(f"/reconflow/directories/{domain}_dirs.json", 'rb'))
+                                  document=open(f"/reconflow/directories/{domain}_dirs.html", 'rb'))
         return domain
